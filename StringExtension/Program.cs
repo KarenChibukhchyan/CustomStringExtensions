af1e5b7a188303ccd45ef0930f6e8f48cc8f8112
Karen Chibukhchyan <karen.chibuxchyan@marktguru.com>
2022-05-29 21:36:05 +0400

    Added custom extension methods to CustomStringExtensions class

--- StringExtension/Program.cs
+++ StringExtension/Program.cs
@@ -6,9 +6,165 @@ namespace StringExtension
     {
         public static void Main(string[] args)
         {
-            string a = "a";
-            string b = "b";
-            Console.WriteLine(a.CustomCompare(b));
+            string s = new String('-', 40);
+            // -------------  CustomCompareTo ------------
+            Console.WriteLine(s, 30);
+            string first = "aa";
+            string second = "ab";
+            var result = first.CustomCompareTo(second);
+            if (result < 0) Console.WriteLine($"{first} precedes {second}");
+            else if (result > 0) Console.WriteLine($"{second} precedes {first}");
+            else Console.WriteLine($"{second} equals {first}");
+            Console.WriteLine();
+
+            // -------------  CustomIsNullOrEmpty ------------
+            Console.WriteLine(s, 30);
+            first = "";
+            second = null;
+            string third = "123";
+            Console.WriteLine($"String {first} is null or empty: {first.CustomIsNullOrEmpty()}");
+            Console.WriteLine($"String {second} is null or empty: {second.CustomIsNullOrEmpty()}");
+            Console.WriteLine($"String {third} is null or empty: {third.CustomIsNullOrEmpty()}");
+            Console.WriteLine();
+
+            // -------------  CustomConcat ------------
+            Console.WriteLine(s, 30);
+            Console.WriteLine($"Concatenation of {first} and {third}:  {first.CustomConcat(third)}");
+            Console.WriteLine($"Concatenation of {third} and {third}:  {third.CustomConcat(third)}");
+            Console.WriteLine();
+
+            // -------------  CustomContains ------------
+            Console.WriteLine(s, 30);
+            first = "aabbcc";
+            second = "abdsd";
+            third = "bbb";
+            string fourth = "cc";
+            Console.WriteLine($"{first} contains {second}: {first.CustomContains(second)}");
+            Console.WriteLine($"{first} contains {third}: {first.CustomContains(third)}");
+            Console.WriteLine($"{first} contains {fourth}: {first.CustomContains(fourth)}");
+            Console.WriteLine();
+
+            // -------------  CustomEquals ------------
+            Console.WriteLine(s, 30);
+            first = "aabbcc";
+            second = "abdsd";
+            third = "aabbcc";
+            Console.WriteLine($"{first} equals {second}: {first.CustomEquals(second)}");
+            Console.WriteLine($"{first} equals {third}: {first.CustomEquals(third)}");
+            Console.WriteLine();
+
+            // -------------  CustomIndexOf ------------
+            Console.WriteLine(s, 30);
+            first = "aabbcc";
+            second = "abb";
+            third = "ccc";
+            Console.WriteLine($"Index of {second} in {first}: {first.CustomIndexOf(second)}");
+            Console.WriteLine($"Index of {third} in {first}: {first.CustomIndexOf(third)}");
+            Console.WriteLine();
+
+            // -------------  CustomIndexOfAny ------------
+            Console.WriteLine(s, 30);
+            first = "aabbcc";
+            second = "wdskjklb";
+            third = "aabbcc";
+            Console.WriteLine($"Index of any {second} in {first}: {first.CustomIndexOfAny(second.ToCharArray())}");
+            Console.WriteLine($"Index of any {third} in {first}: {first.CustomIndexOfAny(third.ToCharArray())}");
+            Console.WriteLine();
+
+            // -------------  CustomInsert ------------
+            Console.WriteLine(s, 30);
+            first = "aabbcc";
+            second = "123";
+            int index = 2;
+            Console.WriteLine($"Insert of {second} in {first} at index {index}: {first.CustomInsert(second, index)}");
+            Console.WriteLine();
+
+            // -------------  CustomJoin ------------
+            Console.WriteLine(s, 30);
+            first = "jjj";
+            second = "123";
+            third = "456";
+            fourth = "+9+";
+            Console.WriteLine($"{second} join {third} with {first}: {first.CustomJoin(second, third, fourth)}");
+            Console.WriteLine();
+
+            // -------------  CustomLastIndexOf ------------
+            Console.WriteLine(s, 30);
+            first = "how do I convert";
+            second = "nve";
+            third = "aabbcc";
+            Console.WriteLine($"Last index of {second} in {first}: {first.CustomLastIndexOf(second)}");
+            Console.WriteLine($"Last index of {third} in {first}: {first.CustomLastIndexOf(third)}");
+            Console.WriteLine();
+
+            // -------------  CustomRemove ------------
+            Console.WriteLine(s, 30);
+            first = "How do I convert a cakes to a beer";
+            int startIndex = 2;
+            int count = 10;
+            Console.WriteLine($"Remove from {first} with start index {startIndex} and {count}: {first.CustomRemove(startIndex, count)}");
+            // count = 60;
+            // Console.WriteLine($"Remove from {first} with start index {startIndex} and {count}: {first.CustomRemove(startIndex, count)}");
+            Console.WriteLine();
+
+            // -------------  CustomReplace ------------
+            Console.WriteLine(s, 30);
+            second = "e";
+            third = "444";
+            Console.WriteLine($"Replace in {first} value {second} with {third}: {first.CustomReplace(second, third)}");
+            second = "eeee";
+            third = "444";
+            Console.WriteLine($"Replace in {first} value {second} with {third}: {first.CustomReplace(second, third)}");
+            Console.WriteLine();
+
+            // -------------  CustomToLower ------------
+            Console.WriteLine(s, 30);
+            first = "aDbCcc3/";
+            second = "abdsd";
+            Console.WriteLine($"{first} to lower: {first.CustomToLower()}");
+            Console.WriteLine($"{second} to lower: {second.CustomToLower()}");
+            Console.WriteLine();
+
+            // -------------  CustomToUpper ------------
+            Console.WriteLine(s, 30);
+            first = "aDbCcc3/";
+            second = "abdsd";
+            Console.WriteLine($"{first} to upper: {first.CustomToUpper()}");
+            Console.WriteLine($"{second} to upper: {second.CustomToUpper()}");
+            Console.WriteLine();
+
+            // -------------  CustomTrim ------------
+            Console.WriteLine(s, 30);
+            first = "  aabbcc  ";
+            second = "1 abdsd      ";
+            Console.WriteLine($"{first} after trimming : {first.CustomTrim()}");
+            Console.WriteLine($"{second} after trimming: {first.CustomTrim()}");
+            Console.WriteLine();
+
+            // -------------  CustomTrimEnd ------------
+            Console.WriteLine(s, 30);
+            first = "  aabbcc  ";
+            second = "1 abdsd      ";
+            Console.WriteLine($"{first} after trimming of end: {first.CustomTrimEnd()}");
+            Console.WriteLine($"{second} after trimming of end: {second.CustomTrimEnd()}");
+            Console.WriteLine();
+
+            // -------------  CustomTrimStart ------------
+            Console.WriteLine(s, 30);
+            first = "  aabbcc  ";
+            second = "1 abdsd      ";
+            Console.WriteLine($"{first} after trimming of start: {first.CustomTrimStart()}");
+            Console.WriteLine($"{second} after trimming of start: {second.CustomTrimStart()}");
+            Console.WriteLine();
+
+            // -------------  CustomSubstring ------------
+            Console.WriteLine(s, 30);
+            first = "Hello World from Substring  ";
+            startIndex = 3;
+            count = 4;
+            Console.WriteLine($"Substring of {first} with start index {startIndex}: {first.CustomSubstring(startIndex)}");
+            Console.WriteLine($"Substring of {first} with start index {startIndex} and count {count}: {first.CustomSubstring(startIndex, count)}");
+            Console.WriteLine();
         }
     }
 }
--- StringExtension/StringExtensions.cs
+++ StringExtension/StringExtensions.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Text;
 
 namespace StringExtension
 {
     public static class CustomStringExtensions
     {
-        public static int CustomCompare(this string thisString, string otherString)
+        public static int CustomCompareTo(this string thisString, string otherString)
         {
             if (thisString.CustomIsNullOrEmpty() || otherString.CustomIsNullOrEmpty())
                 throw new ArgumentException("Cannot compare with empty string");
@@ -22,6 +23,287 @@ namespace StringExtension
         public static bool CustomIsNullOrEmpty(this string thisString)
         {
             return !(thisString?.Length > 0);
+        }
+
+        public static string CustomConcat(this string thisString, string otherString)
+        {
+            if (thisString == null || otherString == null)
+                throw new ArgumentNullException();
+            return thisString + otherString;
+        }
+
+        public static bool CustomContains(this string thisString, string otherString)
+        {
+            if (thisString.CustomIsNullOrEmpty() || otherString.CustomIsNullOrEmpty())
+                throw new ArgumentNullException();
+            if (otherString.Length > thisString.Length)
+                return false;
+            for (int i = 0; i <= thisString.Length - otherString.Length; i++)
+            {
+                if (thisString[i] == otherString[0])
+                {
+                    if (thisString.CustomSubstring(i, otherString.Length) == otherString)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CustomEquals(this string thisString, string otherString)
+        {
+            if (thisString == null || otherString == null)
+                return thisString == otherString;
+
+            if (thisString.Length != otherString.Length)
+                return false;
+
+            for (int i = 0; i < thisString.Length; i++)
+            {
+                if (thisString[i] != otherString[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static int CustomIndexOf(this string thisString, string otherString)
+        {
+            if (thisString == null || otherString == null || thisString == String.Empty || otherString == String.Empty)
+                throw new ArgumentException();
+            if (otherString.Length > thisString.Length)
+                return -1;
+            for (int i = 0; i <= thisString.Length - otherString.Length; i++)
+            {
+                if (thisString[i] == otherString[0])
+                {
+                    bool equals = true;
+                    for (int j = 0; j < otherString.Length; j++)
+                    {
+                        if (thisString[i + j] != otherString[j])
+                            equals = false;
+                    }
+
+                    if (equals)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int CustomIndexOfAny(this string thisString, char[] anyOf)
+        {
+            foreach (var c in anyOf)
+            {
+                var indexOf = thisString.CustomIndexOf(c.ToString());
+                if (indexOf != -1) return indexOf;
+            }
+
+            return -1;
+        }
+
+        public static string CustomInsert(this string thisString, string otherString, int startIndex)
+        {
+            if (thisString == null || otherString == null || startIndex >= thisString.Length)
+                throw new ArgumentException();
+            return thisString.CustomSubstring(0, startIndex) + otherString + thisString.CustomSubstring(startIndex);
+        }
+
+        public static string CustomJoin(this string thisString, params string[] strings)
+        {
+            if (thisString == null)
+                throw new ArgumentException();
+            StringBuilder output = new StringBuilder();
+            foreach (var s in strings)
+            {
+                output.Append($"{s}{thisString}");
+            }
+
+            return output.ToString().CustomSubstring(0, output.Length - thisString.Length);
+        }
+
+        public static int CustomLastIndexOf(this string thisString, string otherString)
+        {
+            if (thisString == null || otherString == null || thisString == String.Empty || otherString == String.Empty)
+                throw new ArgumentException();
+            if (otherString.Length > thisString.Length)
+                return -1;
+            for (int i = thisString.Length - otherString.Length; i >= 0; i--)
+            {
+                if (thisString[i] == otherString[0])
+                {
+                    bool equals = true;
+                    for (int j = 0; j < otherString.Length; j++)
+                    {
+                        if (thisString[i + j] != otherString[j])
+                            equals = false;
+                    }
+
+                    if (equals)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static string CustomSubstring(this string thisString, int startIndex, int count)
+        {
+            if (thisString == null || startIndex < 0 || count < 0 || startIndex > thisString.Length - 1 || startIndex + count > thisString.Length)
+                throw new ArgumentException();
+
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < thisString.Length; i++)
+            {
+                if (i >= startIndex && i < startIndex + count)
+                    output.Append(thisString[i]);
+            }
+
+            return output.ToString();
+        }
+
+        public static string CustomSubstring(this string thisString, int startIndex)
+        {
+            if (thisString == null || startIndex < 0 || startIndex > thisString.Length - 1)
+                throw new ArgumentException();
+            int count = thisString.Length - startIndex;
+            return thisString.CustomSubstring(startIndex, count);
+        }
+
+        public static string CustomRemove(this string thisString, int startIndex, int count)
+        {
+            if (thisString == null || startIndex < 0 || count < 0 || startIndex > thisString.Length - 1 || startIndex + count > thisString.Length)
+                throw new ArgumentException();
+            return thisString.CustomSubstring(0, startIndex) + thisString.CustomSubstring(startIndex + count);
+        }
+
+        public static string CustomReplace(this string thisString, string stringToReplace, string stringToPaste)
+        {
+            if (thisString == null || stringToPaste == null || stringToReplace == null || stringToReplace == String.Empty)
+                throw new ArgumentException();
+            if (!thisString.CustomContains(stringToReplace))
+                return thisString;
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i <= thisString.Length - stringToReplace.Length; i++)
+            {
+                if (thisString[i] == stringToReplace[0])
+                {
+                    bool equals = true;
+                    for (int j = 0; j < stringToReplace.Length; j++)
+                    {
+                        if (thisString[i + j] != stringToReplace[j])
+                            equals = false;
+                    }
+
+                    if (equals)
+                    {
+                        output.Append(stringToPaste);
+                        i = i + stringToReplace.Length - 1;
+                    }
+                }
+                else
+                {
+                    output.Append(thisString[i]);
+                }
+            }
+
+            return output.ToString();
+        }
+
+        public static string CustomToLower(this string thisString)
+        {
+            if (thisString == null)
+                throw new ArgumentException();
+            StringBuilder output = new StringBuilder();
+            foreach (var c in thisString)
+            {
+                int code = c;
+
+                if (code >= 65 && code <= 90)
+                {
+                    output.Append((char) (code + 32));
+                }
+                else
+                {
+                    output.Append(c);
+                }
+            }
+
+            return output.ToString();
+        }
+
+        public static string CustomToUpper(this string thisString)
+        {
+            if (thisString == null)
+                throw new ArgumentException();
+            StringBuilder output = new StringBuilder();
+            foreach (var c in thisString)
+            {
+                int code = c;
+
+                if (code >= 97 && code <= 122)
+                {
+                    output.Append((char) (code - 32));
+                }
+                else
+                {
+                    output.Append(c);
+                }
+            }
+
+            return output.ToString();
+        }
+
+        public static string CustomTrim(this string thisString)
+        {
+            if (thisString.CustomIsNullOrEmpty())
+                throw new ArgumentException();
+            return thisString.CustomTrimStart().CustomTrimEnd();
+        }
+
+        public static string CustomTrimStart(this string thisString)
+        {
+            if (thisString.CustomIsNullOrEmpty())
+                throw new ArgumentException();
+            StringBuilder output = new StringBuilder();
+            int length = thisString.Length;
+            bool startInside = false;
+            for (int i = 0; i < length; i++)
+            {
+                if (!(startInside) && (thisString[i] == ' ' || thisString[i] == '\n' || thisString[i] == '\r' || thisString[i] == '\t'))
+                {
+                    continue;
+                }
+                else
+                {
+                    output.Append(thisString[i]);
+                    startInside = true;
+                }
+            }
+
+            return output.ToString();
+        }
+
+        public static string CustomTrimEnd(this string thisString)
+        {
+            if (thisString.CustomIsNullOrEmpty())
+                throw new ArgumentException();
+            int indexToTrim = -1;
+            for (int i = thisString.Length - 1; i <= 0; i--)
+            {
+                if (thisString[i] == ' ' || thisString[i] == '\n' || thisString[i] == '\r' || thisString[i] == '\t')
+                {
+                    indexToTrim = i;
+                }
+            }
+
+            if (indexToTrim != -1)
+            {
+                return thisString.CustomSubstring(0, indexToTrim);
+            }
+
+            return thisString;
         }
     }
 }
